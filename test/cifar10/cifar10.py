b7a853a1b1ca6bf7d910d21846cf2087a9df433d
Nihal <nsnihalshah@gmail.com>
2016-05-02 15:23:33 -0500

    modified

--- test/cifar10/cifar10.py
+++ test/cifar10/cifar10.py
@@ -290,24 +290,24 @@ def inference(images):
     # Move everything into depth so we can perform a single matrix multiply.
     reshape = tf.reshape(conv3, [FLAGS.batch_size, -1])
     dim = reshape.get_shape()[1].value
-    weights = _variable_with_weight_decay('weights', shape=[dim, 98],
+    weights = _variable_with_weight_decay('weights', shape=[dim, 96],
                                           stddev=0.04, wd=0.004)
-    biases = _variable_on_cpu('biases', [98], tf.constant_initializer(0.1))
+    biases = _variable_on_cpu('biases', [96], tf.constant_initializer(0.1))
     local3 = tf.nn.relu(tf.matmul(reshape, weights) + biases, name=scope.name)
     _activation_summary(local3)
 
   # local4
   with tf.variable_scope('local4') as scope:
-    weights = _variable_with_weight_decay('weights', shape=[98, 49],
+    weights = _variable_with_weight_decay('weights', shape=[96, 48],
                                           stddev=0.04, wd=0.004)
-    biases = _variable_on_cpu('biases', [49], tf.constant_initializer(0.1))
+    biases = _variable_on_cpu('biases', [48], tf.constant_initializer(0.1))
     local4 = tf.nn.relu(tf.matmul(local3, weights) + biases, name=scope.name)
     _activation_summary(local4)
 
   # softmax, i.e. softmax(WX + b)
   with tf.variable_scope('softmax_linear') as scope:
-    weights = _variable_with_weight_decay('weights', [49, NUM_CLASSES],
-                                          stddev=1/49.0, wd=0.0)
+    weights = _variable_with_weight_decay('weights', [48, NUM_CLASSES],
+                                          stddev=1/48.0, wd=0.0)
     biases = _variable_on_cpu('biases', [NUM_CLASSES],
                               tf.constant_initializer(0.0))
     softmax_linear = tf.add(tf.matmul(local4, weights), biases, name=scope.name)
