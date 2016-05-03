33e421a3777093666f9c0aa8911d9b9e51f02165
Nihal <nsnihalshah@gmail.com>
2016-05-03 14:53:14 -0500

    changed numbers

--- test/cifar10/cifar10_eval.py
+++ test/cifar10/cifar10_eval.py
@@ -51,7 +51,7 @@ tf.app.flags.DEFINE_string('eval_data', 'test',
                            """Either 'test' or 'train_eval'.""")
 tf.app.flags.DEFINE_string('checkpoint_dir', 'cifar10_train',
                            """Directory where to read model checkpoints.""")
-tf.app.flags.DEFINE_integer('eval_interval_secs',10,
+tf.app.flags.DEFINE_integer('eval_interval_secs',3,
                             """How often to run the eval.""")
 tf.app.flags.DEFINE_integer('num_examples', 10000,
                             """Number of examples to run.""")
@@ -139,7 +139,7 @@ def evaluate():
 
     summary_writer = tf.train.SummaryWriter(FLAGS.eval_dir, g)
 
-    for i in range(7000):
+    for i in range(300):
       eval_once(saver, summary_writer, top_k_op, summary_op)
       if FLAGS.run_once:
         break
